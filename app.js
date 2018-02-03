39378a8008fb763be4551488828645b3100a3059
unknown <imadeveloper23@gmail.com>
2018-02-03 21:08:30 +0800

    update app.js

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(8000); //the server object listens on port 8080
+}).listen(8000 || process.env.PORT); //the server object listens on port 8080
